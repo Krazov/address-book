df8da17e2170b63c881ca388db9079e9b034e147
Jon Krazov <krazov@interia.pl>
2018-08-12 21:43:41 +0200

    Compiling Less files with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,36 @@
-const path = require('path');
+const path                 = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './app/main.js',
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'bundled')
-    }
-};
+module.exports = [
+    // {
+    //     entry: { app: './app/main.js' },
+    //
+    //     output: {
+    //         filename: 'app.js',
+    //         path:     path.resolve(__dirname, 'bundled'),
+    //     }
+    // },
+    {
+        entry: { style: './app/main.less' },
+
+        output: {
+            path: path.resolve(__dirname, 'css'),
+        },
+
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css',
+            })
+        ],
+
+        module: {
+            rules: [
+                {
+                    test: /\.less$/,
+                    use:  [ MiniCssExtractPlugin.loader, 'css-loader', 'less-loader' ]
+                }
+            ]
+        }
+    },
+];
